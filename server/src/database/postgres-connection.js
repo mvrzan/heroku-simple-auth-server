923be618b5f6f8f4736af50dc08f7078459ad79b
mvrzan <mvrzan@icloud.com>
2025-01-31 15:47:26 -0500

    add PostgreSQL connection pool with SSL configuration and connection logging

---
+++ server/src/database/postgres-connection.js
@@ -0,0 +1,17 @@
+import pg from "pg";
+import { getCurrentTimestamp } from "../utils/getCurrentTimeStamp.js";
+
+const { Pool } = pg;
+
+const rejectUnauthorizedValue = process.env.NODE_ENV === "development" ? false : true;
+
+const postgresPool = new Pool({
+  connectionString: `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+  ssl: { rejectUnauthorized: rejectUnauthorizedValue },
+});
+
+postgresPool.on("connect", () => {
+  console.log(`${getCurrentTimestamp()} 📦 Database successfully connected!`);
+});
+
+export default postgresPool;
