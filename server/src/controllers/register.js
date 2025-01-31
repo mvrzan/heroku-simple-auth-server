04745576cbc07d2bf56988b35d912d6243269ae0
mvrzan <mvrzan@icloud.com>
2025-01-31 14:33:39 -0500

    implement user registration controller with logging and error handling

--- server/src/controllers/register.js
+++ server/src/controllers/register.js
@@ -1,3 +1,19 @@
-const register = () => {};
+import { getCurrentTimestamp } from "../utils/getCurrentTimeStamp.js";
+
+const register = (req, res) => {
+  console.log(`${getCurrentTimestamp()} 📨 Register new user request received...`);
+
+  const username = reg.body?.username;
+  const password = reg.body?.password;
+
+  try {
+    console.log(`${getCurrentTimestamp()} ✅ The user was successfully registered!`);
+
+    res.status(200).json("The user was successfully registered!");
+  } catch (error) {
+    console.error("❌ Error occurred:", error);
+    res.status(500).send(error);
+  }
+};
 
 export default register;
