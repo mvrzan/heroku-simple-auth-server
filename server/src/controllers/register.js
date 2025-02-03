cbc3a765eac771db6755c0a121ec727a0910f85f
mvrzan <mvrzan@icloud.com>
2025-02-03 10:47:19 -0500

    add email validation to user registration process

--- server/src/controllers/register.js
+++ server/src/controllers/register.js
@@ -1,11 +1,19 @@
 import { getCurrentTimestamp } from "../utils/getCurrentTimeStamp.js";
 import createUser from "../database/create-user.js";
+import emailValidator from "../utils/emailValidator.js";
 
 const register = async (req, res) => {
   console.log(`${getCurrentTimestamp()} 📨 Register new user request received...`);
 
   const email = req.body?.email;
   const password = req.body?.password;
+
+  if (!emailValidator(email)) {
+    console.error(`${getCurrentTimestamp()} ❌ Invalid email address: ${email}`);
+    return res.status(400).json({ error: "Invalid email address" });
+  }
+
+  console.log(`${getCurrentTimestamp()} ✉️ Incoming email successfully validated!`);
 
   try {
     await createUser(email, password);
