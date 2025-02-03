1dcb22f0d9040920c827f3159cc82e8febe116d4
mvrzan <mvrzan@icloud.com>
2025-02-03 14:39:09 -0500

    refactor registration process to use isEmailValid and isPasswordValid functions for validation

--- server/src/controllers/register.js
+++ server/src/controllers/register.js
@@ -1,6 +1,7 @@
 import { getCurrentTimestamp } from "../utils/getCurrentTimeStamp.js";
 import createUser from "../database/create-user.js";
-import emailValidator from "../utils/emailValidator.js";
+import isEmailValid from "../utils/emailValidator.js";
+import isPasswordValid from "../utils/passwordValidator.js";
 
 const register = async (req, res) => {
   console.log(`${getCurrentTimestamp()} 📨 Register new user request received...`);
@@ -8,9 +9,16 @@ const register = async (req, res) => {
   const email = req.body?.email;
   const password = req.body?.password;
 
-  if (!emailValidator(email)) {
+  if (!isEmailValid(email)) {
     console.error(`${getCurrentTimestamp()} ❌ Invalid email address: ${email}`);
     return res.status(400).json({ error: "Invalid email address" });
+  }
+
+  const { isValid, errorMessage } = isPasswordValid(password);
+
+  if (!isValid) {
+    console.error(`${getCurrentTimestamp()} ❌ Invalid password`, errorMessage);
+    return res.status(400).json({ error: "Invalid password", errorMessage });
   }
 
   console.log(`${getCurrentTimestamp()} ✉️ Incoming email successfully validated!`);
