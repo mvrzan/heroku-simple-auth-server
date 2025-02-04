0c864543e0e93b32b3d1ebb8c4c9ecba3bd77633
mvrzan <mvrzan@icloud.com>
2025-02-04 14:50:18 -0500

    implement login functionality with email validation and password comparison

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -1,3 +1,39 @@
-const login = () => {};
+import bcrypt from "bcrypt";
+import findUser from "../database/find-user.js";
+import isEmailValid from "../utils/emailValidator.js";
+import { getCurrentTimestamp } from "../utils/getCurrentTimestamp.js";
+
+const login = async (req, res) => {
+  console.log(`${getCurrentTimestamp()} 🔑 Login  request received...`);
+
+  const email = req.body?.email;
+  const password = req.body?.password;
+
+  if (!isEmailValid(email)) {
+    console.error(`${getCurrentTimestamp()} ❌ Invalid email address: ${email}`);
+    return res.status(400).json({ error: `Invalid email address: ${email}` });
+  }
+
+  try {
+    const user = await findUser(email);
+
+    if (!user) {
+      return res.status(400).json({ error: `User not found for email ${email}` });
+    }
+
+    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordsMatch) {
+      console.error(`${getCurrentTimestamp()} ❌ Incorrect password for email: ${email}`);
+      return res.status(403).json({ error: "Incorrect password" });
+    }
+
+    console.log(`${getCurrentTimestamp()} ✅ User login was successful!`);
+    res.status(200).json({ message: "User login was successful!" });
+  } catch (error) {
+    console.error(`${getCurrentTimestamp()} ${error}`);
+    res.status(500).send(error);
+  }
+};
 
 export default login;
