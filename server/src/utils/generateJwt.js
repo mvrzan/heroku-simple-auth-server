24d1115eca92d04ced6df7683d194cb0076c4a8d
mvrzan <mvrzan@icloud.com>
2025-02-05 13:58:33 -0500

    enhance JWT generation with error handling and logging

--- server/src/utils/generateJwt.js
+++ server/src/utils/generateJwt.js
@@ -1,33 +1,41 @@
 import crypto from "crypto";
+import { getCurrentTimestamp } from "./getCurrentTimestamp.js";
 
 const generateJwt = (userEmail) => {
-  const issueTime = new Date();
-  const header = {
-    alg: "HS256",
-    typ: "JWT",
-  };
-  const payload = {
-    iss: process.env.JWT_ISSUER,
-    sub: userEmail,
-    iat: ~~(issueTime.getTime() / 1000),
-    nbf: ~~(issueTime.getTime() / 1000),
-    exp: ~~(issueTime.getTime() / 1000 + 24 * 60 * 60),
-    aud: "",
-  };
-  const sharedSecret = process.env.JWT_SHARED_SECRET;
+  try {
+    const issueTime = new Date();
+    const header = {
+      alg: "HS256",
+      typ: "JWT",
+    };
+    const payload = {
+      iss: process.env.JWT_ISSUER,
+      sub: userEmail,
+      iat: ~~(issueTime.getTime() / 1000),
+      nbf: ~~(issueTime.getTime() / 1000),
+      exp: ~~(issueTime.getTime() / 1000 + 24 * 60 * 60),
+      aud: "frontend",
+    };
+    const sharedSecret = process.env.JWT_SHARED_SECRET;
 
-  const encodedHeader = Buffer.from(JSON.stringify(header)).toString("base64");
+    const encodedHeader = Buffer.from(JSON.stringify(header)).toString("base64");
 
-  const fullPayload = { ...payload };
+    const fullPayload = { ...payload };
 
-  const encodedPayload = Buffer.from(JSON.stringify(fullPayload)).toString("base64");
+    const encodedPayload = Buffer.from(JSON.stringify(fullPayload)).toString("base64");
 
-  const signature = crypto.createHmac("sha256", sharedSecret).update(`${encodedHeader}.${encodedPayload}`).digest();
+    const signature = crypto.createHmac("sha256", sharedSecret).update(`${encodedHeader}.${encodedPayload}`).digest();
 
-  const encodedSignature = signature.toString("base64");
+    const encodedSignature = signature.toString("base64");
 
-  console.log(`${encodedHeader}.${encodedPayload}.${encodedSignature}`);
-  return `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
+    const jwtToken = `${encodedHeader}.${encodedPayload}.${encodedSignature}`;
+
+    console.log(`${getCurrentTimestamp()} 🔐 JWT successfully generated: ${jwtToken}`);
+    return jwtToken;
+  } catch (error) {
+    console.error(`${getCurrentTimestamp()} ❌ An error occurred while generating the JWT:`, error);
+    throw error;
+  }
 };
 
 export default generateJwt;
