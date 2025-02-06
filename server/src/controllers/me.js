cc3c3f1f7fe5e9ca747c2ec87df05b2a3468d434
mvrzan <mvrzan@icloud.com>
2025-02-06 12:56:26 -0500

    implement JWT validation in me controller to enhance security

--- server/src/controllers/me.js
+++ server/src/controllers/me.js
@@ -1,3 +1,20 @@
-const me = () => {};
+import { getCurrentTimestamp } from "../utils/getCurrentTimestamp.js";
+import validateJwt from "../utils/validateJwt.js";
+
+const me = (req, res) => {
+  try {
+    console.log(`${getCurrentTimestamp()} 🧪 Validation request received...`);
+
+    const jwtToken = req.body?.jwtToken;
+
+    validateJwt(jwtToken);
+
+    console.log(`${getCurrentTimestamp()} ✅ JWT successfully validated!`);
+    res.status(200).json({ message: "JWT successfully validated!" });
+  } catch (error) {
+    console.log(`${getCurrentTimestamp()} An error occurred: ${error}`);
+    res.status(500).send(error);
+  }
+};
 
 export default me;
