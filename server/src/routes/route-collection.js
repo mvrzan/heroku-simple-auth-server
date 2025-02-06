14a37fb236fbcaa238f70702b4f4143c697e5ad8
mvrzan <mvrzan@icloud.com>
2025-02-06 15:48:23 -0500

    add validateJwtMiddleware to protect the me route with JWT validation

--- server/src/routes/route-collection.js
+++ server/src/routes/route-collection.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import me from "../controllers/me.js";
 import register from "../controllers/register.js";
 import login from "../controllers/login.js";
+import validateJwtMiddleware from "../middleware/validateJwtMiddleware.js";
 
 const router = Router();
 
 router.post("/v1/login", login);
 router.post("/v1/register", register);
-router.post("/v1/me", me);
+router.post("/v1/me", validateJwtMiddleware, me);
 
 export default router;
